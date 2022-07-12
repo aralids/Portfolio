c00e4adb812b8fb54d705dc24084429977dd6412
Dilara Sarach <dilara.sarach@abv.bg>
2022-07-12 23:50:40 +0200

    Sloppy implementation of additional grids: To be refined

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ function gridImage(name, scheme) {
     this.scheme = scheme;
     this.makeCurrent = function() {
         if (cellSchemeCurrent.length === 0) {
-            let cellGrid = document.getElementById('cell-grid');
+            let cellGrid = document.getElementById("cell-grid");
+            let card1 = document.getElementById("card1");
+            let card2 = document.getElementById("card2");
             for (let i = 0; i < 17; i++) {
+                /* For cellGrid: */
                 let div = document.createElement("div");
                 div.setAttribute("id", `row-${i}`);
                 cellGrid.appendChild(div);
+
+                /* For card1: */
+                let div1 = document.createElement("div");
+                div1.setAttribute("id", `card1-row-${i}`);
+                card1.appendChild(div1);
+
+                /* For card2: */
+                let div2 = document.createElement("div");
+                div2.setAttribute("id", `card2-row-${i}`);
+                card2.appendChild(div2);
             }
             for (let i = 0; i < 17; i++) {
                 for (let j = 0; j < 15; j++) {
+                    /* For cellGrid: */
                     let btn = document.createElement("button");
                     let id = `cell-${i}-${j}`;
                     btn.setAttribute("id", `cell-${i}-${j}`);
-                    btn.classList.add("cell", `row-${i}`, `coll-${j}`, this.scheme[i*15+j], "dead");
+                    btn.classList.add("cell", `row-${i}`, `col-${j}`, this.scheme[i*15+j], "dead");
                     btn.setAttribute("onmouseover", `resurrect('${id}')`);
                     document.getElementById(`row-${i}`).appendChild(btn);
+
+                    /* For card1: */
+                    let btn1 = document.createElement("button");
+                    let id1 = `card1-cell-${i}-${j}`;
+                    btn1.setAttribute("id", `card1-cell-${i}-${j}`);
+                    btn1.classList.add("cell", `card1-row-${i}`, `card1-col-${j}`, this.scheme[i*15+j], "alive");
+                    btn1.setAttribute("onmouseover", `resurrect('${id1}')`);
+                    document.getElementById(`card1-row-${i}`).appendChild(btn1);
+
+                    /* For card2: */
+                    let btn2 = document.createElement("button");
+                    let id2 = `card2-cell-${i}-${j}`;
+                    btn2.setAttribute("id", `card2-cell-${i}-${j}`);
+                    btn2.classList.add("cell", `card2-row-${i}`, `card2-col-${j}`, this.scheme[i*15+j], "alive");
+                    btn2.setAttribute("onmouseover", `resurrect('${id2}')`);
+                    document.getElementById(`card2-row-${i}`).appendChild(btn2);
                 }
             }
             return this.scheme;
@@ -68,6 +98,8 @@ function calculateDistance(cellSchemeCurrent, cellSchemeNext) {
     return differences;
 }
 
+
+
 /* -- GRID COLOR SCHEMES -- */
 
 
@@ -91,11 +123,19 @@ function updateCellGrid() {
         cellSchemeCurrent = gridInitial.makeCurrent();
     }
     if (winScroll > 537 && winScroll < 1075) {
-        document.getElementById("cell-grid").style.left = rect.left + "px";;
+        document.getElementById("cell-grid").style.left = rect.left + "px";
+        document.getElementById("card1").style.left = rect.left + "px";
+        document.getElementById("card1").style.boxShadow = "none";
+        document.getElementById("card2").style.left = rect.left + "px";
+        document.getElementById("card2").style.boxShadow = "none";
         cellSchemeCurrent = grid1.makeCurrent();
     }
     if (winScroll > 1075) {
         document.getElementById("cell-grid").style.left = "calc(97% - 370px)";
+        document.getElementById("card1").style.left = "calc(57% - 370px)";
+        document.getElementById("card1").style.boxShadow = "0px 0px 20px 5px #A9A9A9";
+        document.getElementById("card2").style.left = "calc(77% - 370px)";
+        document.getElementById("card2").style.boxShadow = "0px 0px 20px 5px #A9A9A9";
     }
     console.log(calculateDistance(cellSchemeInitial, cellScheme1));
 }

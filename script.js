2d40023e4daf613b30e0fe7ee2f200bf3f3688e3
Dilara Sarach <dilara.sarach@abv.bg>
2022-07-14 23:56:58 +0200

    Step towards switching living states

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let gridHeight = 21;
 let gridWidth = 21;
 
-function gridImage(name, scheme, gridId, initialLivingState) {
+function gridImage(name, scheme, gridId) {
     this.name = name;
     this.scheme = scheme;
     this.gridId = gridId;
-    this.initialLivingState = initialLivingState;
-    this.makeCurrent = function(gridSchemeCurrent) {
+    this.makeCurrent = function(gridSchemeCurrent, livingState) {
         if (gridSchemeCurrent.length === 0) {
             let grid = document.getElementById(this.gridId);
             for (let i = 0; i < gridHeight; i++) {
@@ -19,7 +18,7 @@ function gridImage(name, scheme, gridId, initialLivingState) {
                     let btn = document.createElement("button");
                     let id = `${this.gridId}-cell-${i}-${j}`;
                     btn.setAttribute("id", `${this.gridId}-cell-${i}-${j}`);
-                    btn.classList.add("cell", `${this.gridId}-row-${i}`, `${this.gridId}-col-${j}`, this.scheme[i*gridWidth+j], this.initialLivingState);
+                    btn.classList.add("cell", `${this.gridId}-row-${i}`, `${this.gridId}-col-${j}`, this.scheme[i*gridWidth+j], livingState);
                     btn.setAttribute("onmouseover", `resurrect('${id}')`);
                     document.getElementById(`${this.gridId}-row-${i}`).appendChild(btn);
                 }
@@ -30,7 +29,11 @@ function gridImage(name, scheme, gridId, initialLivingState) {
             differences = calculateDistance(gridSchemeCurrent, this.scheme);
             for (let i = 0; i < differences.length; i++) {
                 let cell = document.getElementById(`${this.gridId}-cell-${Math.floor(differences[i][0] / gridWidth)}-${differences[i][0] % gridWidth}`);
-                cell.classList.replace(cell.classList[3], differences[i][1])
+                cell.classList.replace(cell.classList[3], differences[i][1]);
+            }
+            for (let i = 0; i < this.scheme.length; i++) {
+                let cell = document.getElementById(`${this.gridId}-cell-${Math.floor(i / gridWidth)}-${i % gridWidth}`);
+                cell.classList.replace(cell.classList[4], livingState);
             }
             return this.scheme;
         }
@@ -132,15 +135,15 @@ let cellSchemeCard1 = [];
 let cellSchemeCard2 = [];
 
 
-gridInitial = new gridImage("cellSchemeInitial", cellSchemeInitial, "cell-grid", "dead");
-grid1 = new gridImage("cellScheme1", cellScheme1, "cell-grid", "alive");
-gridCard1 = new gridImage("cellCard1", cellScheme1, "card1", "alive");
-gridCard2 = new gridImage("cellCard2", cellScheme1, "card2", "alive");
-qrCodeGrid = new gridImage("qrCode", qrCode, "cell-grid", "alive")
+gridInitial = new gridImage("cellSchemeInitial", cellSchemeInitial, "cell-grid");
+grid1 = new gridImage("cellScheme1", cellScheme1, "cell-grid");
+gridCard1 = new gridImage("cellCard1", cellScheme1, "card1");
+gridCard2 = new gridImage("cellCard2", cellScheme1, "card2");
+qrCodeGrid = new gridImage("qrCode", qrCode, "cell-grid")
 
-cellSchemeCurrent = gridInitial.makeCurrent(cellSchemeCurrent);
-cellSchemeCard1 = gridCard1.makeCurrent(cellSchemeCard1);
-cellSchemeCard2 = gridCard2.makeCurrent(cellSchemeCard2);
+cellSchemeCurrent = gridInitial.makeCurrent(cellSchemeCurrent, "dead");
+cellSchemeCard1 = gridCard1.makeCurrent(cellSchemeCard1, "alive");
+cellSchemeCard2 = gridCard2.makeCurrent(cellSchemeCard2, "alive");
 
 
 let viewportHeight = window.innerHeight;
@@ -154,22 +157,22 @@ function updateCellGrid() {
     console.log("Here");
     var winScroll = document.getElementById("information").scrollTop || document.documentElement.scrollTop;
     console.log(winScroll);
-    if (winScroll >= 0 && winScroll < viewportHeight*(4/5) - 1) {
-        cellSchemeCurrent = gridInitial.makeCurrent(cellSchemeCurrent);
+    if (winScroll >= 0 && winScroll < viewportHeight*(4/5) - 5) {
+        cellSchemeCurrent = gridInitial.makeCurrent(cellSchemeCurrent, "dead");
 
         console.log("WELCOME", 0)
     }
-    if (winScroll > viewportHeight*(4/5) - 1 && winScroll < 2*(viewportHeight*(4/5)) - 1) {
+    if (winScroll > viewportHeight*(4/5) - 5 && winScroll < 2*(viewportHeight*(4/5)) - 5) {
         document.getElementById("cell-grid").style.left = rect.left + "px";
         document.getElementById("card1").style.left = rect.left + "px";
         document.getElementById("card1").style.boxShadow = "none";
         document.getElementById("card2").style.left = rect.left + "px";
         document.getElementById("card2").style.boxShadow = "none";
-        cellSchemeCurrent = grid1.makeCurrent(cellSchemeCurrent);
+        cellSchemeCurrent = grid1.makeCurrent(cellSchemeCurrent, "alive");
 
         console.log("ABOUT-ME", viewportHeight*(4/5))
     }
-    if (winScroll > 2*(viewportHeight*(4/5) - 1) && winScroll < 3*(viewportHeight*(4/5)) - 1) {
+    if (winScroll > 2*(viewportHeight*(4/5) - 5) && winScroll < 3*(viewportHeight*(4/5)) - 5) {
         document.getElementById("cell-grid").style.left = "calc(97% - 490px)";
         document.getElementById("card1").style.left = "calc(77% - 490px)";
         document.getElementById("card1").style.boxShadow = "0px 0px 20px 5px #A9A9A9";
@@ -178,9 +181,9 @@ function updateCellGrid() {
 
         console.log("PROJECTS", 2*(viewportHeight*(4/5)))
     }
-    if (winScroll > 3*(viewportHeight*(4/5)) - 1) {
+    if (winScroll > 3*(viewportHeight*(4/5)) - 5) {
         document.getElementById("cell-grid").style.left = rect.left + "px";
-        cellSchemeCurrent = qrCodeGrid.makeCurrent(cellSchemeCurrent);
+        cellSchemeCurrent = qrCodeGrid.makeCurrent(cellSchemeCurrent, "alive");
 
         document.getElementById("card1").style.left = rect.left + "px";
         document.getElementById("card1").style.boxShadow = "none";
@@ -197,13 +200,13 @@ function changeLivingState(id) {
     if (aliveState === "alive") {
         document.getElementById(id).classList.replace("alive", "dead");
     } else {
-        document.getElementById(id).classList.replace("dead", "alive");
+        document.getElementById(id).classList.replace("dead");
     }
 }
 
 function resurrect(id) {
     let aliveState = document.getElementById(id).classList[4];
     if (aliveState === "dead") {
-        document.getElementById(id).classList.replace("dead", "alive");
+        document.getElementById(id).classList.replace("dead");
     }
 }

4418cb5660b81f6bb9b97b43c680e6215503f322
Dilara Sarach <dilara.sarach@abv.bg>
2022-08-26 21:24:28 +0200

    Infinite scroll loop based on setInterval()!

--- portfolio/firstedition/static/js/vitaminsscript.js
+++ portfolio/firstedition/static/js/vitaminsscript.js
@@ -1,18 +1,9 @@
 $(document).ready(function (){
-    $('#reel-1').scrollTop(725);
-    console.log($('.slot').outerHeight());
-    console.log($('.reel').outerHeight());
-    console.log($('.slot').outerHeight()*4);
-    $('#reel-1').on('scroll', function () {
+    setInterval(function(){ 
         if ($('#reel-1').scrollTop() < 201.1) {
             $('#reel-1').scrollTop($('#reel-1').scrollTop() + 804.4);
         }else if ($('#reel-1').scrollTop() >= 804.4) {
             $('#reel-1').scrollTop($('#reel-1').scrollTop() - 804.4);
         }
-        console.log($('#reel-1').scrollTop());
-    })
-    for (let i = 0; i < 20; i++) {
-        let x = $('#reel-1').scrollTop();
-        $('#reel-1').animate({ scrollTop: (x + i*201.1) % 804.4 }, 1000);
-    }
+      }, 40);
 })

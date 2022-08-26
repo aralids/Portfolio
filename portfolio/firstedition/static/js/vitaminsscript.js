2e411ec0661365a4df45b21dc4ffb8a222defb4e
Dilara Sarach <dilara.sarach@abv.bg>
2022-08-26 19:36:04 +0200

    Pixels adjusted for full-screen

--- portfolio/firstedition/static/js/vitaminsscript.js
+++ portfolio/firstedition/static/js/vitaminsscript.js
@@ -4,10 +4,10 @@ $(document).ready(function (){
     console.log($('.reel').outerHeight());
     console.log($('.slot').outerHeight()*4);
     $('#reel-1').on('scroll', function () {
-        if ($('#reel-1').scrollTop() < 181) {
-            $('#reel-1').scrollTop($('#reel-1').scrollTop() + 725.5999755859375);
-        }else if ($('#reel-1').scrollTop() >= 725.5999755859375) {
-            $('#reel-1').scrollTop($('#reel-1').scrollTop() - 725.5999755859375);
+        if ($('#reel-1').scrollTop() < 201.1) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() + 804.4);
+        }else if ($('#reel-1').scrollTop() >= 804.4) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() - 804.4);
         }
         console.log($('#reel-1').scrollTop());
     })

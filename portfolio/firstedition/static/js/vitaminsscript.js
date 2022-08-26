65dc8736efd70a1aad8d943d9de84396855bfa33
Dilara Sarach <dilara.sarach@abv.bg>
2022-08-26 19:22:49 +0200

    Scroll loop goes forward & backwards now :)

--- portfolio/firstedition/static/js/vitaminsscript.js
+++ portfolio/firstedition/static/js/vitaminsscript.js
@@ -1,13 +1,14 @@
 $(document).ready(function (){
-    console.log($('#reel-1').height());
-    console.log($('.slot').height());
-    var y = $('#reel-1').scrollTop();  //your current y position on the page
-    console.log(y);
-    $('#reel-1').scrollTop(y+4*181.33);
-    console.log($('#reel-1').scrollTop());
+    $('#reel-1').scrollTop(725);
+    console.log($('.slot').outerHeight());
+    console.log($('.reel').outerHeight());
+    console.log($('.slot').outerHeight()*4);
     $('#reel-1').on('scroll', function () {
-        if ($('#reel-1').scrollTop() >= 753.5999755859375) {
-            $('#reel-1').scrollTop($('#reel-1').scrollTop() - 753.5999755859375);
+        if ($('#reel-1').scrollTop() < 181) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() + 725.5999755859375);
+        }else if ($('#reel-1').scrollTop() >= 725.5999755859375) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() - 725.5999755859375);
         }
+        console.log($('#reel-1').scrollTop());
     })
 })

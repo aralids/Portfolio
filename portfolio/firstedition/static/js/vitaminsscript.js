2fdf78a4acdb71453818df4da68c854392933a4f
Dilara Sarach <dilara.sarach@abv.bg>
2022-08-28 23:58:00 +0200

    Fixed flickering bug

--- portfolio/firstedition/static/js/vitaminsscript.js
+++ portfolio/firstedition/static/js/vitaminsscript.js
@@ -1,9 +1,22 @@
 $(document).ready(function (){
+    let slotHeight = $('.slot').outerHeight();
+    let slotHeight4 = $('.slot').outerHeight()*4;
+    let reel = $('.reel').outerHeight();
+    $('#reel-1').scrollTop(slotHeight);
+    console.log("slot: ", slotHeight)
+    console.log("slot x 4: ", slotHeight4)
+    console.log("reel: ", reel)
     setInterval(function(){ 
-        if ($('#reel-1').scrollTop() < 201.1) {
-            $('#reel-1').scrollTop($('#reel-1').scrollTop() + 804.4);
-        }else if ($('#reel-1').scrollTop() >= 804.4) {
-            $('#reel-1').scrollTop($('#reel-1').scrollTop() - 804.4);
+        if ($('#reel-1').scrollTop() < slotHeight) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() + slotHeight4);
+            window.setTimeout(function () {
+                disableScroll = false;
+            }, 40);
+        } else if ($('#reel-1').scrollTop() >= slotHeight * 5) {
+            $('#reel-1').scrollTop($('#reel-1').scrollTop() - slotHeight4);
+            window.setTimeout(function () {
+                disableScroll = false;
+            }, 40);
         }
-      }, 40);
+    }, 40);
 })

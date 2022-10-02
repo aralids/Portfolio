2df6b4caafd8a60782f995c0313f9bcf0de25500
Dilara Sarach <dilara.sarach@abv.bg>
2022-10-02 03:17:10 +0200

    Color choosing, clear & save buttons

--- portfolio/firstedition/static/js/symbolsscript.js
+++ portfolio/firstedition/static/js/symbolsscript.js
@@ -4,6 +4,55 @@ canvas.height = main.offsetHeight;
 canvas.width = main.offsetWidth;
 const ctx = canvas.getContext("2d");
 
-// lets create a rectangle for testing purposes
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 75, 75);
+let prevX = null
+let prevY = null
+
+ctx.lineWidth = 5
+
+let draw = false
+let clrs = document.querySelectorAll(".clr")
+clrs = Array.from(clrs)
+clrs.forEach(clr => {
+    clr.addEventListener("click", () => {
+        ctx.strokeStyle = clr.dataset.clr
+    })
+})
+
+let clearBtn = document.querySelector(".clear")
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+// Saving drawing as image
+let saveBtn = document.querySelector(".save")
+saveBtn.addEventListener("click", () => {
+    let data = canvas.toDataURL("imag/png")
+    let a = document.createElement("a")
+    a.href = data
+    // what ever name you specify here
+    // the image will be saved as that name
+    a.download = "sketch.png"
+    a.click()
+})
+
+window.addEventListener("mousedown", (e) => draw = true)
+window.addEventListener("mouseup", (e) => draw = false)
+
+window.addEventListener("mousemove", (e) => {
+    if(prevX == null || prevY == null || !draw){
+        prevX = e.clientX
+        prevY = e.clientY - 75
+        return
+    }
+
+    let currentX = e.clientX
+    let currentY = e.clientY - 75
+
+    ctx.beginPath()
+    ctx.moveTo(prevX, prevY)
+    ctx.lineTo(currentX, currentY)
+    ctx.stroke()
+
+    prevX = currentX
+    prevY = currentY
+})

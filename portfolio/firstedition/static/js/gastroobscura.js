8566c86c437f36817bad06d1256ad0b5ec8fc2ed
Dilara Sarach <dilara.sarach@abv.bg>
2022-12-08 17:10:53 +0100

    clip-path is the solution for what I have in mind! :D

--- portfolio/firstedition/static/js/gastroobscura.js
+++ portfolio/firstedition/static/js/gastroobscura.js
@@ -67,25 +67,6 @@ getLocation();
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
 
 function Place(props) {
     console.log("props: ", (String(props.left) + "px"))

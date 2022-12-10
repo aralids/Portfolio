574670ccf60e0660f55f71212c0a2ecc104a8d59
Dilara <50795069+aralids@users.noreply.github.com>
2022-12-10 09:18:52 +0100

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
-    'api'
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
